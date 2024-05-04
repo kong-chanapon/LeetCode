1575b430a8b548a3de9eb3603a41102c69a92f6a
chanapon chuenchuwit <71110346+63070028@users.noreply.github.com>
2024-05-04 22:06:17 +0700

    Time: 1367 ms (24.61%), Space: 0B (100%) - LeetHub

---
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+select c.customer_id
+from Customer c
+join Product p
+on c.product_key = p.product_key
+group by c.customer_id
+having count(distinct(p.product_key)) = (select count(*) from Product)
