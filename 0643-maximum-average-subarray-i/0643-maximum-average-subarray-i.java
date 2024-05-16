0e757449b8c83251a4f78fa8cece0b5da0375196
chanapon chuenchuwit <71110346+63070028@users.noreply.github.com>
2024-05-16 16:40:22 +0700

    Time: 8 ms (10.43%), Space: 56.5 MB (15.32%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,45 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        // double ans = Double.NEGATIVE_INFINITY;
+        //Time Out
+        // int start = 0;
+        // double sum = 0;
+        // for(int end = k-1; end<nums.length; end++){
+        //     int index = start;
+        //     while(index <= end){
+        //         sum += nums[index++];
+        //     }
+        //     ans = Math.max(ans, sum/k);
+        //     sum = 0;
+        //     start++;
+        // }
+
+        double[] prefixSum = new double[nums.length];
+        prefixSum[0] = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            prefixSum[i] = nums[i] + prefixSum[i-1];
+        }
+
+        double ans = prefixSum[k-1]/k;
+        int start = 0;
+        for(int end=k; end<nums.length; end++){
+            ans = Math.max(ans, (prefixSum[end] - prefixSum[start])/k);
+            start++;
+        }
+
+        return ans;    
+    }
+}
+
+//nums = [1,12,-5,-6,50,3], n=6, k = 4
+
+//s1 = [1,12,-5,-6] index 0-3 = 2
+//s2 = [12,-5,-6,50] index 1-4 = 51
+//s3 = [-5,-6,50,3] index 2-5 = 42
+
+//prefixSum [1, 13, 8, 2, 52, 55]
+//begin index 0-3 -> prefixSum[k-1]
+// index 1-4 -> prefixSum[4] - prefixSum[0]
+// index 2-5 -> prefixSum[5] - prefixSum[2]
+
